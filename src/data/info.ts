513043c3fe9a7de91c0d4f2059f8868a1f12b703
Hernan Diaz <hernandiaz035@gmail.com>
2023-12-20 15:37:00 +1100

    Start mocking up data

---
+++ src/data/info.ts
@@ -0,0 +1,50 @@
+export const info = {
+	navbar: {
+		title: 'Hedior'
+	},
+	footer: {
+		title: 'Hedior'
+	},
+	portfolio: {
+		name: 'Hernan Diaz',
+		jobDescription: 'Full-Stack Web Developer',
+		about: `I'm a Software Developer Sydney, Australia.....`,
+
+		experience: [
+			{
+				name: 'PALO IT',
+				location: 'Sydney, Australia',
+				startDate: 'November, 2021',
+				endDate: 'December, 2023',
+				description: []
+			}
+		],
+
+		education: [
+			{
+				name: 'University EIA',
+				location: 'Medellin, Colombia',
+				startDate: '2013',
+				endDate: '2018',
+				description: ['Bachelor of Science, Mechatronic Engineering']
+			}
+		],
+
+		socialMedia: {
+			facebook: '',
+			github: '',
+			email: '',
+			linkedin: ''
+		},
+
+		projects: [
+			{
+				title: '',
+				isFeatured: true,
+				thumbnail: '',
+				githubUrl: '',
+				liveUrl: ''
+			}
+		]
+	}
+}
