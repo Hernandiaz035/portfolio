abf9bdbff11f94521cefd2f9eebfb0dd7d2b8f1b
Hernan Diaz <hernandiaz035@gmail.com>
2023-12-21 13:00:37 +1100

    Create Navbar with Tailwind config for DarkMode

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	darkMode: 'class',
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				'hd-black': '#1f1f1f',
+				'hd-grey': '#4d4d4d',
+				'hd-white': '#ebebeb',
+				'hd-blue': '#80a8ff',
+				'hd-dark-blue': '#296dff'
+			}
+		}
 	},
-	plugins: [],
-};
+	plugins: []
+}
