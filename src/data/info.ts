7762d29f5dbb607ff30237caea824b09d4c7645a
Hernan Diaz <hernandiaz035@gmail.com>
2023-12-22 14:14:35 +1100

    Scafold app and add information.

--- src/data/info.ts
+++ src/data/info.ts
@@ -8,39 +8,63 @@ export const info = {
 	portfolio: {
 		name: 'Hernan Diaz',
 		jobDescription: 'Full-Stack Web Developer',
-		about: `I'm a Software Developer Sydney, Australia.....`,
+		about: `I'm a full-stack developer located in Sydney, Australia, with a strong background in
+		engineering and a passion for innovative web development. My main skills are in React, Next.js,
+		NodeJS, and TypeScript, ideal for both front-end and back-end development tasks.
+		Consulting has equipped me with effective communication skills and the ability to tailor
+		solutions to client needs. I am enthusiastic about technology and always eager to learn,
+		thriving in settings where I can explore new technologies and frameworks. I enjoy being challenged
+		and am always ready to tackle complex projects that push the boundaries of my expertise.`,
 
 		experience: [
 			{
 				name: 'PALO IT',
+				jobTitle: 'Full-Stack Web Developer',
 				location: 'Sydney, Australia',
 				startDate: 'November, 2021',
 				endDate: 'December, 2023',
-				description: []
+				jobDescription: `As a Technical Consultant at Palo IT, my role as a full-stack developer involved
+				working closely with clients to deliver customized web solutions across various industries.
+				I specialized in enhancing user interfaces, migrating applications to efficient frameworks,
+				and adapting functionalities to meet specific client needs. This position also enhanced my
+				ability to collaborate effectively within diverse project teams.`,
+				achievements: [
+					'Migrated an application from JSP to AWS Lambda and Angular 16 for AMP Limited, significantly reducing infrastructure costs by 80%.',
+					'Developed key features for Singapore GovTech RBS’s reservation and booking platform',
+					'Engineered a CMS-driven website for MA Financial, showcasing products and team members across various regions.',
+					'Enhanced user interaction and process efficiency for the HSBC NTB Experience reducing dropout over 10%.',
+					'Implemented core functionalities in an NFT marketplace for BlockTrust.',
+					'Built a robust sales gateway for MunichRE CyRUSS in the cyber security insurance sector for the Asia Pacific market.'
+				]
 			}
 		],
 
-		education: [
+		educationList: [
 			{
 				name: 'University EIA',
 				location: 'Medellin, Colombia',
 				startDate: '2013',
 				endDate: '2018',
-				description: ['Bachelor of Science, Mechatronic Engineering']
+				description: 'Bachelor of Science, Mechatronic Engineering'
+			},
+			{
+				name: 'Warwick Institute',
+				location: 'Sydney, Australia',
+				startDate: '2020',
+				endDate: '2021',
+				description: 'Advanced diploma in Information Technology'
 			}
 		],
 
 		socialMedia: {
-			facebook: '',
-			github: '',
-			email: '',
-			linkedin: ''
+			github: 'https://github.com/hernandiaz035',
+			linkedin: 'https://www.linkedin.com/in/hernandiaz035/'
 		},
 
 		projects: [
 			{
 				title: '',
-				isFeatured: true,
+				isFeatured: false,
 				thumbnail: '',
 				githubUrl: '',
 				liveUrl: ''
